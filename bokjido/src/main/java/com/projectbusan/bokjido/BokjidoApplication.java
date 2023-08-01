8f70cdf9f266c64db9881861f6615af5e7df95cb
Mintchodan <z78z@naver.com>
2023-08-01 11:09:18 +0900

    feat: 초기프로젝트

---
+++ bokjido/src/main/java/com/projectbusan/bokjido/BokjidoApplication.java
@@ -0,0 +1,13 @@
+package com.projectbusan.bokjido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokjidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokjidoApplication.class, args);
+	}
+
+}
---
+++ bokjido/src/test/java/com/projectbusan/bokjido/BokjidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectbusan.bokjido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokjidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
