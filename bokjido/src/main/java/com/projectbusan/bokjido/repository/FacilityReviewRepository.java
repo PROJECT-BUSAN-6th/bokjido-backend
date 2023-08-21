135347222b8f9b51151b62956e9df3d9d0b9e499
Mintchodan <z78z@naver.com>
2023-08-21 17:19:43 +0900

    feat[#20]: 건물 리뷰 리포지토리 생성

--- bokjido/src/main/java/com/projectbusan/bokjido/repository/FacilityReviewRepository.java
+++ bokjido/src/main/java/com/projectbusan/bokjido/repository/FacilityReviewRepository.java
@@ -1,2 +1,16 @@
-package com.projectbusan.bokjido.repository;public interface FacilityReviewRepository {
+package com.projectbusan.bokjido.repository;
+
+import com.projectbusan.bokjido.dto.FacilityReviewDTO;
+import com.projectbusan.bokjido.entity.FacilityReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FacilityReviewRepository extends JpaRepository<FacilityReview, Long> {
+//    @Query("SELECT m FROM FacilityReview m WHERE m.facility.id = :facilityId")
+    List<FacilityReview> findByFacilityId(Long facilityId);
 }
