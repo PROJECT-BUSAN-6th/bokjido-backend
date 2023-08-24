718322b40b9c0675c4f3a81ce01e9ad9868f15d9
Mintchodan <z78z@naver.com>
2023-08-24 10:11:56 +0900

    feat[#23]: User Entity 항목 추가

--- bokjido/src/main/java/com/projectbusan/bokjido/entity/User.java
+++ bokjido/src/main/java/com/projectbusan/bokjido/entity/User.java
@@ -1,6 +1,8 @@
 package com.projectbusan.bokjido.entity;
 
 import com.projectbusan.bokjido.dto.AuthDTO;
+import com.projectbusan.bokjido.enums.HouseholdSituationCategory;
+import com.projectbusan.bokjido.enums.InterestTopicCategory;
 import com.projectbusan.bokjido.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
@@ -26,9 +28,19 @@ public class User {
 
     private String password;
 
+    private String email;
+
+    private String phone;
+
     private LocalDate birth;
 
     private String gender;
+
+    @Enumerated(EnumType.STRING)
+    private HouseholdSituationCategory householdSituationCategory;
+
+    @Enumerated(EnumType.STRING)
+    private InterestTopicCategory interestTopicCategory;
 
     private Role role;
 
@@ -37,13 +49,17 @@ public class User {
     private LocalDateTime modify_date;
 
     @Builder
-    public User(Long id, String userid, String username, String password, LocalDate birth, String gender, Role role, LocalDateTime create_date, LocalDateTime modify_date) {
+    public User(Long id, String userid, String username, String password, String email, String phone, LocalDate birth, String gender, HouseholdSituationCategory householdSituationCategory, InterestTopicCategory interestTopicCategory, Role role, LocalDateTime create_date, LocalDateTime modify_date) {
         this.id = id;
         this.userid = userid;
         this.username = username;
         this.password = password;
+        this.email = email;
+        this.phone = phone;
         this.birth = birth;
         this.gender = gender;
+        this.householdSituationCategory = householdSituationCategory;
+        this.interestTopicCategory = interestTopicCategory;
         this.role = role;
         this.create_date = create_date;
         this.modify_date = modify_date;
@@ -54,8 +70,12 @@ public class User {
                 .userid(signupDto.getUserid())
                 .password(signupDto.getPassword())
                 .username(signupDto.getUsername())
+                .email(signupDto.getEmail())
+                .phone(signupDto.getPhone())
                 .birth(signupDto.getBirth())
                 .gender(signupDto.getGender())
+                .householdSituationCategory(signupDto.getHouseholdSituationCategory())
+                .interestTopicCategory(signupDto.getInterestTopicCategory())
                 .role(Role.USER)
                 .create_date(LocalDateTime.now())
                 .build();
